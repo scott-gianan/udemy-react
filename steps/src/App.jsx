3a7fa7b267e7f490afe0256b20067d3ebfbd1341
scott-gianan <scott.gianan@gmail.com>
2023-06-22 14:36:01 +0800

    initial commit

--- steps/src/App.jsx
+++ steps/src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [step, setStep] = useState(0);
+  const handlePreviousStep = () => {
+    if (step === 0) {
+      setStep((prevStep) => 2);
+    } else {
+      setStep((prevStep) => prevStep - 1);
+    }
+  };
+  const handleNextStep = () => {
+    if (step === 2) {
+      setStep((prevStep) => 0);
+    } else {
+      setStep((prevStep) => prevStep + 1);
+    }
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="steps">
+      <div className="numbers">
+        <div className={step === 0 ? "active" : null}>1</div>
+        <div className={step === 1 ? "active" : null}>2</div>
+        <div className={step === 2 ? "active" : null}>3</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <p className="message">{messages[step]}</p>
+      <div className="buttons">
+        <button onClick={handlePreviousStep} className="btn">
+          Previous
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button onClick={handleNextStep} className="btn">
+          Next
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
