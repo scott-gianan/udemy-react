fcd556989a96aa1affb3e7d9bfc9815df38d3a1b
scott-gianan <scott.gianan@gmail.com>
2023-06-24 16:56:51 +0800

    initial

---
+++ travel-list/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Logo from "./Components/Logo/Logo";
+import Form from "./Components/Form/Form";
+import PackingList from "./Components/PackingList/PackingList";
+import Stats from "./Components/Stats/Stats";
+
+function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+export default App;
