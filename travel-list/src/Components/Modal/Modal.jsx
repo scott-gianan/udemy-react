05dd620129fa0e722f7c23ab47991674f2ac654c
scott-gianan <scott.gianan@gmail.com>
2023-06-26 16:49:29 +0800

    added more functionality to modal

--- travel-list/src/Components/Modal/Modal.jsx
+++ travel-list/src/Components/Modal/Modal.jsx
@@ -1,16 +1,36 @@
+import { useState, useEffect } from "react";
 import "./Modal.scss";
 
 function Modal({ clearList, closeModal }) {
+  console.log("modal rendered");
+  const [toggleModal, setToggleModal] = useState(true);
+  const backdropCloseModal = () => {
+    setToggleModal((currentValue) => !currentValue);
+  };
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      const escapeKey = event.key === "Escape";
+      if (escapeKey) {
+        setToggleModal((currentValue) => !currentValue);
+      }
+    };
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
   return (
-    <div className="backdrop">
-      <div className="modal">
-        <h3>Do you want to clear your list?</h3>
-        <div className="button-container">
-          <button onClick={clearList}>✔️</button>
-          <button onClick={closeModal}>❌</button>
+    toggleModal && (
+      <div className="backdrop" onClick={backdropCloseModal}>
+        <div className="modal">
+          <h3>Do you want to clear your list?</h3>
+          <div className="button-container">
+            <button onClick={clearList}>✔️</button>
+            <button onClick={closeModal}>❌</button>
+          </div>
         </div>
       </div>
-    </div>
+    )
   );
 }
 export default Modal;
