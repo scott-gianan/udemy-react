496d64213d2dc8daf2ccbb1069185a1ed4c17d29
scott-gianan <scott.gianan@gmail.com>
2023-07-04 14:45:38 +0800

    refactor. final

--- eat-and-split/src/Components/Bill/Bill.jsx
+++ eat-and-split/src/Components/Bill/Bill.jsx
@@ -1,58 +1,72 @@
 import { useState } from "react";
 import Button from "../Button/Button";
-function Bill({ isToggled, selectedFriend }) {
-  const [totalBill, setTotalBill] = useState({
-    bill: 0,
-    "your-expense": 0,
-    payee: true,
-  });
-  const currentBill = Number(totalBill.bill + totalBill["your-expense"]);
-  const friendExpense = Number(totalBill.bill - totalBill["your-expense"]);
-
+function Bill({ isToggled, selectedFriend, setFriends, setToggleBill }) {
+  const [bill, setBill] = useState("");
+  const [myExpense, setMyExpense] = useState("");
+  const [payee, setPayee] = useState(true);
   const { id, balance, name } = selectedFriend;
-  const handleOnChange = (event) => {
-    if (Math.abs(friendExpense) > currentBill) {
-      console.log("exceeded");
+  const friendExpense = Number(bill - myExpense);
+  const handleChangeBill = (event) => {
+    setBill(() => Number(event.target.value));
+  };
+  const handleMyExpense = (event) => {
+    const expense = Number(event.target.value);
+    if (expense > bill) {
+      return;
     }
-    setTotalBill((currBill) => {
-      return {
-        ...currBill,
-        [event.target.name]:
-          event.target.name === "payee"
-            ? event.target.value
-            : Number(event.target.value),
-      };
-    });
+    setMyExpense(() => expense);
+  };
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if (payee) {
+      setFriends((currFriends) => {
+        return currFriends.map((friend) => {
+          return friend.id === id
+            ? { ...friend, balance: balance + friendExpense, isSelected: false }
+            : friend;
+        });
+      });
+    } else {
+      setFriends((currFriends) => {
+        return currFriends.map((friend) => {
+          return friend.id === id
+            ? { ...friend, balance: balance + myExpense, isSelected: false }
+            : friend;
+        });
+      });
+    }
+    setToggleBill((v) => !v);
+    setBill("");
+    setMyExpense("");
+    setPayee(true);
   };
   return (
     isToggled && (
-      <form className="form-split-bill">
+      <form className="form-split-bill" onSubmit={handleOnSubmit}>
         <h2>Split a bill with {name}</h2>
         <label htmlFor="bill">💰 Bill value</label>
         <input
           id="bill"
-          name="bill"
           type="number"
-          value={totalBill.bill}
-          onChange={handleOnChange}
+          value={bill}
+          onChange={handleChangeBill}
         />
         <label htmlFor="your-expense">🧍‍♀️ Your expense</label>
         <input
           id="your-expense"
           type="number"
-          name="your-expense"
-          value={totalBill["your-expense"]}
-          // value={
-          //   totalBill.bill < totalBill["your-expense"]
-          //     ? totalBill.bill
-          //     : totalBill["your-expense"]
-          // }
-          onChange={handleOnChange}
+          value={myExpense}
+          onChange={handleMyExpense}
         />
         <label>👫 {name}' expense</label>
         <input disabled type="number" value={friendExpense} />
         <label htmlFor="payee">🤑 Who is paying the bill ❓</label>
-        <select id="payee" name="payee" onChange={handleOnChange}>
+        <select
+          id="payee"
+          name="payee"
+          value={payee}
+          onChange={(e) => setPayee(e.target.value)}
+        >
           <option value={true}>You</option>
           <option value={false}>{name}</option>
         </select>
