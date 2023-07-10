3b9fd98bc0ca951459ea30dcc84d4a2fbc22af22
scott-gianan <scott.gianan@gmail.com>
2023-07-10 17:49:33 +0800

    insertion to Main Component

--- usepopcorn/src/App.jsx
+++ usepopcorn/src/App.jsx
@@ -1,19 +1,14 @@
 //Components
 import Navbar from "./Components/Navigation/Navbar";
-import ListBox from "./Components/Main/ListBox/ListBox";
-import WatchedBox from "./Components/Main/WatchedBox/WatchedBox";
+import Main from "./Components/Main/Main";
 //react hooks
-import { useState } from "react";
 import "./App.css";
 
 export default function App() {
   return (
     <>
       <Navbar />
-      <main className="main">
-        <ListBox />
-        <WatchedBox />
-      </main>
+      <Main />
     </>
   );
 }
