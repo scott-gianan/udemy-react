7558b1f66846aef69d56747b8072078ebb702bfa
scott-gianan <scott.gianan@gmail.com>
2023-07-10 17:42:31 +0800

    placed movie summary to its own component

--- usepopcorn/src/Components/Main/WatchedBox/WatchedBox.jsx
+++ usepopcorn/src/Components/Main/WatchedBox/WatchedBox.jsx
@@ -3,45 +3,21 @@ import { useState } from "react";
 //Components
 import ToggleButton from "../../../Components/ToggleButton/ToggleButton";
 import WatchedMovieList from "./WatchedMovieList/WatchedMovieList";
+import WatchedSummary from "./WatchedSummary/WatchedSummary";
 //custom hooks
 import useToggle from "../../../Hooks/useToggle";
 //assets
-import { tempMovieData } from "../../../assets/tempMovieData";
-console.log(tempMovieData);
+import { tempWatchedMovieData } from "../../../assets/tempWatchedMovieData";
 function WatchedBox() {
-  const [watched, setWatched] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedMovieData);
   const [isOpen, toggle] = useToggle();
-  const average = (arr) =>
-    arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
-  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
-  const avgUserRating = average(watched.map((movie) => movie.userRating));
-  const avgRuntime = average(watched.map((movie) => movie.runtime));
+
   return (
     <div className="box">
       <ToggleButton onClick={toggle}>{isOpen ? "–" : "+"}</ToggleButton>
       {isOpen && (
         <>
-          <div className="summary">
-            <h2>Movies you watched</h2>
-            <div>
-              <p>
-                <span>#️⃣</span>
-                <span>{watched.length} movies</span>
-              </p>
-              <p>
-                <span>⭐️</span>
-                <span>{avgImdbRating}</span>
-              </p>
-              <p>
-                <span>🌟</span>
-                <span>{avgUserRating}</span>
-              </p>
-              <p>
-                <span>⏳</span>
-                <span>{avgRuntime} min</span>
-              </p>
-            </div>
-          </div>
+          <WatchedSummary watchedMovies={watched} />
           <WatchedMovieList watchedMovies={watched} />
         </>
       )}
