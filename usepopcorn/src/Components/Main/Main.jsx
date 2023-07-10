5fc3d8d335c5f023f43748af6da40c45a9d24e69
scott-gianan <scott.gianan@gmail.com>
2023-07-10 17:48:05 +0800

    initial

---
+++ usepopcorn/src/Components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import ListBox from "./ListBox/ListBox";
+import WatchedBox from "./WatchedBox/WatchedBox";
+function Main() {
+  return (
+    <main className="main">
+      <ListBox />
+      <WatchedBox />
+    </main>
+  );
+}
+
+export default Main;
