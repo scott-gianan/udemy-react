6036ef3728fafb5e7faaf06c3e0b4da60b3a80ac
scott-gianan <scott.gianan@gmail.com>
2023-07-11 19:14:10 +0800

    used Box component

--- usepopcorn/src/App.jsx
+++ usepopcorn/src/App.jsx
@@ -1,14 +1,37 @@
-//Components
-import Navbar from "./Components/Navigation/Navbar";
-import Main from "./Components/Main/Main";
 //react hooks
+import { useState } from "react";
+//Components
+import Box from "./Components/Box/Box";
+import Navbar from "./Components/Navigation/Navbar";
+import Search from "./Components/Navigation/Search/Search";
+import NumResults from "./Components/Navigation/NumResults/NumResults";
+import MovieList from "./Components/Main/ListBox/MovieList/MovieList";
+import Main from "./Components/Main/Main";
+import WatchedSummary from "./Components/Main/WatchedBox/WatchedSummary/WatchedSummary";
+import WatchedMovieList from "./Components/Main/WatchedBox/WatchedMoviesList/WatchedMoviesList";
+//css
 import "./App.css";
-
+//assets
+import { tempMovieData } from "./assets/tempMovieData";
+import { tempWatchedMovieData } from "./assets/tempWatchedMovieData";
 export default function App() {
+  const [movies, setMovies] = useState(tempMovieData);
+  const [watchedMovies, setWatchedMovies] = useState(tempWatchedMovieData);
   return (
     <>
-      <Navbar />
-      <Main />
+      <Navbar>
+        <Search />
+        <NumResults numOfMovies={movies.length} />
+      </Navbar>
+      <Main>
+        <Box>
+          <MovieList movies={movies} />
+        </Box>
+        <Box>
+          <WatchedSummary watchedMovies={watchedMovies} />
+          <WatchedMovieList watchedMovies={watchedMovies} />
+        </Box>
+      </Main>
     </>
   );
 }
