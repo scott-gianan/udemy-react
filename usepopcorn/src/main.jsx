bb1e211ce4c73c292e14927d91e5496e22c095a1
scott-gianan <scott.gianan@gmail.com>
2023-07-17 10:30:48 +0800

    removed StarRating

--- usepopcorn/src/main.jsx
+++ usepopcorn/src/main.jsx
@@ -1,15 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import StarRating from "./Components/StarRating/StarRating";
-// import App from './App.jsx'
-// import './index.css'
+import App from "./App.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
-      maxRating={5}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-    />
+    <App />
   </React.StrictMode>
 );
