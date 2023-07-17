2caa260e896698523cccc8dd3214e72d60bdebb1
scott-gianan <scott.gianan@gmail.com>
2023-07-17 10:30:36 +0800

    added onSubmit

--- usepopcorn/src/Components/Navigation/Search/Search.jsx
+++ usepopcorn/src/Components/Navigation/Search/Search.jsx
@@ -1,15 +1,22 @@
 import { useState } from "react";
 
-function Search() {
+function Search({ onHandleMovieSearch }) {
   const [query, setQuery] = useState("");
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+    onHandleMovieSearch(query);
+    setQuery("");
+  };
   return (
-    <input
-      className="search"
-      type="text"
-      placeholder="Search movies..."
-      value={query}
-      onChange={(e) => setQuery(e.target.value)}
-    />
+    <form onSubmit={handleOnSubmit}>
+      <input
+        className="search"
+        type="text"
+        placeholder="Search movies..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </form>
   );
 }
 
