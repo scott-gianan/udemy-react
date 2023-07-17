27a1272b51600897906b8495aaa0310e6f3d1442
scott-gianan <scott.gianan@gmail.com>
2023-07-17 10:31:03 +0800

    added handleMovieSearch

--- usepopcorn/src/App.jsx
+++ usepopcorn/src/App.jsx
@@ -1,5 +1,5 @@
 //react hooks
-import { useState } from "react";
+import { useEffect, useState } from "react";
 //Components
 import Box from "./Components/Box/Box";
 import Navbar from "./Components/Navigation/Navbar";
@@ -9,22 +9,38 @@ import MovieList from "./Components/Main/ListBox/MovieList/MovieList";
 import Main from "./Components/Main/Main";
 import WatchedSummary from "./Components/Main/WatchedBox/WatchedSummary/WatchedSummary";
 import WatchedMovieList from "./Components/Main/WatchedBox/WatchedMoviesList/WatchedMoviesList";
+import Loader from "./Components/Loader/Loader";
+
 //css
 import "./App.css";
 //assets
 import { tempMovieData } from "./assets/tempMovieData";
 import { tempWatchedMovieData } from "./assets/tempWatchedMovieData";
+//constant
+const KEY = "5abe5097";
 export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
+  const [movies, setMovies] = useState([]);
   const [watchedMovies, setWatchedMovies] = useState(tempWatchedMovieData);
+  useEffect(() => {}, []);
+  const handleMovieSearch = async (query) => {
+    const response = await fetch(
+      `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+    );
+    const data = await response.json();
+    if (data.Response === "True") {
+      setMovies([...data.Search]);
+    }
+  };
+
   return (
     <>
       <Navbar>
-        <Search />
+        <Search onHandleMovieSearch={handleMovieSearch} />
         <NumResults numOfMovies={movies.length} />
       </Navbar>
       <Main>
         <Box>
+          <Loader />
           <MovieList movies={movies} />
         </Box>
         <Box>
