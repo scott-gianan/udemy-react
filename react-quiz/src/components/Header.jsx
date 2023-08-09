d43ab592c833b169ac9ab48c3e18ded7bc74ca4d
scott-gianan <scott.gianan@gmail.com>
2023-08-09 13:01:22 +0800

    added logo

--- react-quiz/src/components/Header.jsx
+++ react-quiz/src/components/Header.jsx
@@ -1,7 +1,8 @@
+import reactLogo from "../assets/logo512.png";
 function Header() {
   return (
-    <header className='app-header'>
-      <img src='logo512.png' alt='React logo' />
+    <header className="app-header">
+      <img src={reactLogo} alt="React logo" />
       <h1>The React Quiz</h1>
     </header>
   );
