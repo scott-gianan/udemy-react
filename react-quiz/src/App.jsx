5a975815bdfe095c169211c9f3366172691c3f60
scott-gianan <scott.gianan@gmail.com>
2023-08-09 19:01:10 +0800

    initial

--- react-quiz/src/App.jsx
+++ react-quiz/src/App.jsx
@@ -1,7 +1,72 @@
+//components
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Loader from "./components/Loader";
+import Error from "./components/Error";
+import StartScreen from "./components/StartScreen";
+import Question from "./components/Question";
 import "./App.css";
-
+import { useEffect, useReducer } from "react";
+const initialState = {
+  questions: [],
+  //status: loading, error, ready, active, finished
+  status: "loading",
+  index: 0,
+  answer: null,
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error", message: action.payload };
+    case "start":
+      return { ...state, status: "active" };
+    case "newAnswer":
+      return { ...state, answer: action.payload };
+    default:
+      throw new Error("Unknown Action");
+  }
+};
 function App() {
-  return <>hi</>;
+  const [{ questions, status, message, index, answer }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+  useEffect(() => {
+    async function fetchQuestions() {
+      try {
+        const response = await fetch("http://localhost:8000/questions");
+        const data = await response.json();
+        if (!data) {
+          throw new Error("Error fetching data");
+        }
+        dispatch({ type: "dataReceived", payload: data });
+      } catch (error) {
+        dispatch({ type: "dataFailed", payload: error.message });
+      }
+    }
+    fetchQuestions();
+  }, []);
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error err={message} />}
+        {status === "ready" && (
+          <StartScreen quizItemsLength={questions.length} dispatch={dispatch} />
+        )}
+        {status === "active" && (
+          <Question
+            currentQuestion={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
+      </Main>
+    </div>
+  );
 }
 
 export default App;
