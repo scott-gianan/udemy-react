f170580b30eb15e21cc145a383ba16f5de5ec850
scott-gianan <scott.gianan@gmail.com>
2023-08-11 09:03:36 +0800

    added emoji

--- react-quiz/src/components/FinishScreen.jsx
+++ react-quiz/src/components/FinishScreen.jsx
@@ -1,13 +1,22 @@
 import RestartQuizBtn from "./RestartQuizBtn";
 
-function FinishScreen({ score, dispatch, maxTotalPoints }) {
+function FinishScreen({ score, dispatch, maxTotalPoints, highScore }) {
   const percentage = (score / maxTotalPoints) * 100;
+  let emoji;
+  if (percentage === 100) emoji = "🥇";
+  if (percentage >= 90 && percentage < 100) emoji = "🥇";
+  if (percentage >= 80 && percentage < 90) emoji = "🥈";
+  if (percentage >= 70 && percentage < 80) emoji = "🥉";
+  if (percentage >= 1 && percentage < 70) emoji = "👍";
+  if (percentage === 0) emoji = "🤦";
+
   return (
     <>
       <p className="result">
-        You scored <strong>{score}</strong> out of {maxTotalPoints} points (
-        {Math.ceil(percentage)}%)
+        {emoji} You scored <strong>{score}</strong> out of {maxTotalPoints}{" "}
+        points ({Math.ceil(percentage)}%)
       </p>
+      <p className="highscore">(Highscore : {highScore} points)</p>
       <RestartQuizBtn dispatch={dispatch} />
     </>
   );
