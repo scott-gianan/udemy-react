621586f98241d75df4ef0defd4c92b0aac9c1bda
scott-gianan <scott.gianan@gmail.com>
2023-08-17 14:00:39 +0800

    moved to public folder

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,5 +1,12 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+//components
+import Homepage from "./pages/Homepage";
 function App() {
-  return <>world wise</>;
+  return (
+    <>
+      <Homepage />
+    </>
+  );
 }
 
 export default App;
--- worldwise/src/pages/Homepage.jsx
+++ worldwise/src/pages/Homepage.jsx
@@ -1,4 +1,4 @@
-import styles from "./Homepage.module.css";
+import styles from "./styles/Homepage.module.css";
 
 export default function Homepage() {
   return (
